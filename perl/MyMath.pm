84d33d3c7db2802956cd6b7744988a281858b8b5
torako-san <gldemo1207@gmail.com>
2025-02-27 12:16:04 +0900

    add pm

---
+++ perl/MyMath.pm
@@ -0,0 +1,16 @@
+package MyMath;
+
+use strict;
+use warnings;
+
+sub add {
+    my ($a, $b) = @_;
+    return $a + $b;
+}
+
+sub subtract {
+    my ($a, $b) = @_;
+    return $a - $b;
+}
+
+1; # モジュールの終わりに必要
